d1c890f3dc63c61dffbdde72ed01e57c6d200411
demil133 <demil133@gmail.com>
2013-03-04 19:49:58 +0100

    FIX: install script now with colors!

--- make.sh
+++ make.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dot-files
-############################
-
-########## Variables
-dir=`pwd`	                    # dotfiles directory
-olddir=dotfiles_old             # old dotfiles backup directory
-files=".bash_aliases .i3 .vim .virmc .bashrc .gitconfig .vimrc"    # list of files/folders to symlink in homedir
-# create dotfiles_old in homedir
-mkdir -p ~/$olddir
-# change to the dotfiles directory
-cd $dir
-# if file is a link it unlinks
-# if is a file it backs up
-# then it links to my dotfile
+olddir=${HOME}/dotfiles_old             # old dotfiles backup directory
+mkdir -p $olddir
+files=".bash_aliases .i3 .vim .virmc .bashrc .gitconfig .vimrc"   
 for file in $files; do
     if [ -L ~/$file ];
     then
 	unlink ~/$file
-	echo "[relink] $file"
+	echo -e "\033[34m [relink]\033[0m $file \e[0m"
     else
 	if [ -f ~/$file ];
-	then
-	    echo "[backup] $file"
-	    mv ~/$file ~/$olddir/$file
-	else
-	    echo "[linked] $file"
+	    then
+	    	mv -f ~/$file ~/$olddir/$file
+	    	echo -e "\033[32m [backup]\033[0m $file \e[0m"
+	    else
+	  	echo -e "\033[33m [linked]\033[0m $file"
 	fi
     fi
-    ln -s $dir/$file ~/$file
+    ln -s $file ${HOME}/$file
 done
