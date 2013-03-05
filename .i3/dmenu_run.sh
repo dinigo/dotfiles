fee8936cce3b3ff2122ce71bc3917adbb483d1c1
Daniel I <demil133@gmail.com>
2013-03-05 18:01:25 +0100

    FIX: changes dmenu_path to new way of invoking it

--- .i3/dmenu_run.sh
+++ .i3/dmenu_run.sh
@@ -1,1 +1,2 @@
-$(dmenu_path | dmenu -i -fn "-*-helvetica-medium-r-*-*-21-*-*-*-*-*-*-*" -sb '#000000' -nb '#772953' -nf '#FFFFFF')
+#$(dmenu_cache | dmenu -i -fn Monospace:regular-15 -sb '#000000' -nb '#772953' -nf '#FFFFFF')
+dmenu_run -i -fn Monospace:regular-$1 -sb '#000000' -nb '#772953' -nf '#FFFFFF'
