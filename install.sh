c3c2f4cef47bae79420496d9e47e860262f44ef1
Daniel I <demil133@gmail.com>
2013-03-11 00:23:18 +0100

    FIX: Solves error on backup
    Creates backup folder it it doesn't exist inside the function'

--- install.sh
+++ install.sh
@@ -1,24 +1,45 @@
 #!/bin/bash
+
+# This function sets your configuration relinking and 
+# making backups when necesary
+backupFile()
+{
+    if [ -L $2 ]; then
+	unlink $2
+	echo -e "\033[34m [relink]\033[0m $2 "
+    elif [ -e $2 ]; then
+	if [ ! -d $3 ]; then
+	    DIRNAME=`dirname "$3"`
+	    mkdir -p $DIRNAME
+	fi
+	mv -f $2 $3 
+	echo -e "\033[32m [backup]\033[0m $2 "
+    else
+	echo -e "\033[33m [linked]\033[0m $2"
+    fi
+    ln -s $1 $2
+}
+
 olddir=dotfiles_old
 mkdir -p ${HOME}/$olddir
-files=".bash_aliases .i3 .vim .virmc .bashrc .gitconfig .vimrc"   
+# restores home dotfiles
 for file in .* ; do
     case $file in
 	.|..|.git)
 	    continue
 	    ;;
 	*)
-	    if [ -L ${HOME}/$file ]; then
-		unlink ${HOME}/$file
-		echo -e "\033[34m [relink]\033[0m $file \e[0m"
-	    elif [ -e ${HOME}/$file ]; then
-		mv -f ${HOME}/$file ${HOME}/$olddir/$file
-		echo -e "\033[32m [backup]\033[0m $file \e[0m"
-	    else
-		echo -e "\033[33m [linked]\033[0m $file"
-	    fi
-	    ln -s `pwd`/$file ${HOME}
+	    backupFile `pwd`/$file ${HOME}/$file ${HOME}/$olddir/$file 
 	    ;;
     esac
 done
+
+# loads aliases
 source ${HOME}/.bash_aliases
+
+# link .config/ files for openbox and tint2
+backupFile `pwd`/tint2rc ${HOME}/.config/tint2/tint2rc ${HOME}/.config/tint2/tint2rc/$olddir/tint2rc
+backupFile `pwd`/tint2rc ${HOME}/.config/tint2/tint2rc ${HOME}/.config/tint2/$olddir/tint2rc 
+backupFile `pwd`/rc.xml ${HOME}/.config/openbox/rc.xml ${HOME}/.config/openbox/$olddir/rc.xml
+backupFile `pwd`/autostart ${HOME}/.config/openbox/autostart ${HOME}/.config/openbox/$olddir/autostart
+
