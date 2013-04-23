f061e361c0f19e809790ec3548bd42eb80ea51a1
Daniel I <demil133@gmail.com>
2013-04-23 03:05:57 +0200

    FIX: now increase and decrese brightness with '++' and '--'

--- .scripts/set_brightness.sh
+++ .scripts/set_brightness.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-echo $1 | sudo tee /sys/class/backlight/intel_backlight/brightness
+BRIG=`cat /sys/class/backlight/intel_backlight/brightness`
+BRIG_MAX=`cat /sys/class/backlight/intel_backlight/brightness`
+
+echo "    current: $BRIG"
+echo "    max lvl: $BRIG_MAX"
+
+case $1 in
+    "++" )
+        BRIG=$(($BRIG+100))
+        continue;;
+    "--" )
+        BRIG=$(($BRIG-100))
+        continue;;
+    * )
+        echo "set_brightness.sh [level]"
+        echo "    level: should be greater than 0 and less than"
+        echo "           the number specified by your inter driver"
+        continue;;
+esac
+
+echo "    new lvl: $BRIG"
+
+if [[ $BRIG -le $BRIG_MAX && $BRIG -gt 0 ]]; then
+    echo $BRIG | sudo tee /sys/class/backlight/intel_backlight/brightness
+fi
